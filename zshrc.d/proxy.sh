6658dcd04b00d3ef0fb3b3d4ae809f162467781d
hpyhacking <crazyflapjack@gmail.com>
2015-09-25 18:45:17 +0800

    support proxychains4 for proxy in terminal

--- zshrc.d/proxy.sh
+++ zshrc.d/proxy.sh
@@ -1,9 +1,2 @@
-function set_proxy {
-  export HTTP_PROXY='127.0.0.1:1080'
-  export HTTPS_PROXY='127.0.0.1:1080'
-}
-
-function unset_proxy {
-  unset HTTP_PROXY
-  unset HTTPS_PROXY
-}
+export PROXYCHAINS_CONF_FILE="~/.proxychains.conf"
+alias fuck="proxychains4 -q"
