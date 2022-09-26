54d14aea47098f4adcbf510675ca2e6d7e2b59a0
hpyhacking <github@hpyhacking.me>
2022-09-26 05:25:29 -0400

    update

---
+++ config/zshrc.d/pros.sh
@@ -0,0 +1,1 @@
+alias pros=prosv5
--- config/zshrc.d/ruby.sh
+++ config/zshrc.d/ruby.sh
@@ -1,8 +1,8 @@
 # Fixed octopress generate
-export LC_ALL="en_US.UTF-8"
+# export LC_ALL="en_US.UTF-8"
 
 # Load rbenv automatically by adding
 # the following to ~/.zshrc:
 #
-export PATH="/usr/local/sbin:/usr/local/opt/rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+# export PATH="/usr/local/sbin:/usr/local/opt/rbenv/bin:$PATH"
+# eval "$(rbenv init -)"
